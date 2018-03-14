c287c376d055207094406a3624ccd4270739a55f
sonmaximum <maxjuchheim@gmail.com>
2018-03-14 03:25:30 -0400

    Add location_id to route serializer
    Pass the location_id to route requests

--- app/serializers/route_serializer.rb
+++ app/serializers/route_serializer.rb
@@ -2,7 +2,7 @@
 
 class RouteSerializer < ActiveModel::Serializer
   attributes :id, :name, :rating, :route_type, :attempted, :completed, :sent,
-             :project, :hangs, :comments, :color
+             :project, :hangs, :comments, :color, :location_id
 
   belongs_to :user
   belongs_to :location
